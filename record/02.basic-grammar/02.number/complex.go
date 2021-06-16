a674705be7aef1f0ff74e48bf2e116fc4f95eeef
vyron <fiee_vyron@163.com>
2021-06-16 19:02:52 +0800

    docs: 学习复数类型

--- record/02.basic-grammar/02.number/complex.go
+++ record/02.basic-grammar/02.number/complex.go
@@ -1,12 +1,45 @@
 package main
 
+import (
+	"fmt"
+)
+
 /*
 
 复数: 由两个浮点数表示,其中一个表示实部(real), 一个表示虚部(imag)
 
-类型: complex128(64位实数和虚数|复数的默认类型) complex64(32位实数和虚数)
-
 组成: RE+IMi RE表示实数部分 IM表示虚数部分 i表示虚数单位
 
+类型: complex128(64位实数和虚数|复数的默认类型) complex64(32位实数和虚数)
+
+声明:
+
+var name complex128 = complex(x,y);
+
+name := complex(x,y);
+
+x: 表示复数实数部分
+
+y: 表示实数的虚数部分
+
+
+获取实数和虚数:
+
+var plural complex64 = complex(10,2);
+
+var _real = real(plural);
+
+var _imag = imag(plural);
 
 */
+
+func main(){
+	var plural complex64 = complex(10,2);
+
+	var _real = real(plural);
+
+	var _imag = imag(plural);
+
+	fmt.Println(_real);
+	fmt.Println(_imag);
+}
